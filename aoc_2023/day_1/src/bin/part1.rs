7b1887a9f2960861ccf0c4697c2d1f5457e15f4d
LukaDev <47296785+lukadev-0@users.noreply.github.com>
2023-12-01 20:48:51 +0100

    AOC 2023: Day 1

---
+++ aoc_2023/day_1/src/bin/part1.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let input = include_str!("./part1_input.txt");
+    println!("{}", part_1(input));
+}
+
+fn part_1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut digits = line.chars().filter(|char| char.is_numeric()).peekable();
+
+            format!(
+                "{}{}",
+                digits.peek().unwrap().clone(),
+                digits.next_back().unwrap()
+            )
+            .parse::<u32>()
+            .unwrap()
+        })
+        .sum()
+}
---
+++ aoc_2023/day_1/src/bin/part2.rs
@@ -0,0 +1,39 @@
+use regex::Regex;
+
+fn main() {
+    let input = include_str!("./part1_input.txt");
+    println!("{}", part_2(input));
+}
+
+fn part_2(input: &str) -> u32 {
+    let re = Regex::new("[0-9]|one|two|three|four|five|six|seven|eight|nine").unwrap();
+
+    input
+        .lines()
+        .map(|line| {
+            let mut digits = re
+                .captures_iter(line)
+                .map(|capture| match &capture[0] {
+                    "one" => 1,
+                    "two" => 2,
+                    "three" => 3,
+                    "four" => 4,
+                    "five" => 5,
+                    "six" => 6,
+                    "seven" => 7,
+                    "eight" => 8,
+                    "nine" => 9,
+                    capture => capture.parse::<u32>().unwrap(),
+                })
+                .peekable();
+
+            format!(
+                "{}{}",
+                digits.peek().unwrap().clone(),
+                digits.last().unwrap()
+            )
+            .parse::<u32>()
+            .unwrap()
+        })
+        .sum()
+}
